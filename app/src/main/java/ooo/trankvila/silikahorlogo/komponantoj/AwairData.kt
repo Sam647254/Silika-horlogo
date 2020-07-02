77d278e4e03f201661739c15f04cd91d3a008827
Sam Wang <smwn@hey.com>
2020-07-01 23:17:17 -0700

    Aldonis nivelojn al Awair

--- app/src/main/java/ooo/trankvila/silikahorlogo/komponantoj/AwairData.kt
+++ app/src/main/java/ooo/trankvila/silikahorlogo/komponantoj/AwairData.kt
@@ -4,22 +4,33 @@ import androidx.compose.Composable
 import androidx.ui.core.Alignment
 import androidx.ui.core.Modifier
 import androidx.ui.foundation.Text
+import androidx.ui.graphics.Color
 import androidx.ui.layout.*
 import androidx.ui.text.SpanStyle
 import androidx.ui.text.annotatedString
 import androidx.ui.unit.dp
 import androidx.ui.unit.sp
 import ooo.trankvila.silikahorlogo.AwairData
-import ooo.trankvila.silikahorlogo.ui.Saira
-import ooo.trankvila.silikahorlogo.ui.shadow
+import ooo.trankvila.silikahorlogo.ui.*
 
 @Composable
-fun AwairDataField(value: String, label: String, unit: String = "") {
+fun <T : Comparable<T>> AwairDataField(
+    value: T,
+    label: String,
+    unit: String = "",
+    normalRange: ClosedRange<T>,
+    warning1Range: ClosedRange<T>
+) {
+    val color = when (value) {
+        in normalRange -> Color.White
+        in warning1Range -> if (value > normalRange.endInclusive) warning3 else warning1
+        else -> if (value > warning1Range.endInclusive) warning4 else warning2
+    }
     Column(horizontalGravity = Alignment.CenterHorizontally) {
         Text(
             annotatedString {
                 pushStyle(SpanStyle(fontSize = 50.sp))
-                append(value)
+                append(if (value is Int) value.toString() else "%.1f".format(value))
                 if (unit.isNotEmpty()) {
                     pop()
                     pushStyle(SpanStyle(fontSize = 25.sp))
@@ -31,14 +42,16 @@ fun AwairDataField(value: String, label: String, unit: String = "") {
             style = shadow,
             modifier = Modifier.padding(horizontal = 15.dp),
             maxLines = 1,
-            softWrap = false
+            softWrap = false,
+            color = color
         )
         Text(
             label,
             fontSize = 20.sp,
             fontFamily = Saira,
             style = shadow,
-            modifier = Modifier.offset(y = (-10).dp)
+            modifier = Modifier.offset(y = (-10).dp),
+            color = color
         )
     }
 }
@@ -50,33 +63,45 @@ fun AwairDataStrip(data: AwairData) {
         horizontalArrangement = Arrangement.Center
     ) {
         AwairDataField(
-            value = data.score.toString(),
-            label = "Score"
+            value = data.score,
+            label = "Score",
+            normalRange = 80..100,
+            warning1Range = 60..100
         )
         AwairDataField(
-            value = "%.1f".format(data.temperature),
+            value = data.temperature,
             label = "Temperature",
-            unit = "°C"
+            unit = "°C",
+            normalRange = 18.0..25.0,
+            warning1Range = 16.0..30.0
         )
         AwairDataField(
-            value = "%.1f".format(data.humidity),
+            value = data.humidity,
             label = "Humidity",
-            unit = "%"
+            unit = "%",
+            normalRange = 35.0..55.0,
+            warning1Range = 20.0..65.0
         )
         AwairDataField(
-            value = data.co2.toString(),
+            value = data.co2,
             label = "Carbon dioxide",
-            unit = "PPM"
+            unit = "PPM",
+            normalRange = 0..600,
+            warning1Range = 0..1000
         )
         AwairDataField(
-            value = data.voc.toString(),
+            value = data.voc,
             label = "V.O.C.",
-            unit = "PPB"
+            unit = "PPB",
+            normalRange = 0..333,
+            warning1Range = 0..1000
         )
         AwairDataField(
-            value = data.pm25.toString(),
+            value = data.pm25,
             label = "PM2.5",
-            unit = "μg/m³"
+            unit = "μg/m³",
+            normalRange = 0..20,
+            warning1Range = 0..55
         )
     }
 }
--- app/src/main/java/ooo/trankvila/silikahorlogo/ui/Color.kt
+++ app/src/main/java/ooo/trankvila/silikahorlogo/ui/Color.kt
@@ -8,6 +8,11 @@ val purple500 = Color(0xFF6200EE)
 val purple700 = Color(0xFF3700B3)
 val teal200 = Color(0xFF03DAC5)
 
+val warning1 = Color(0xFFf2c47c)
+val warning2 = Color(0xFFFb9105)
+val warning3 = Color(0xFFf09595)
+val warning4 = Color(0xFFc92222)
+
 val weekdayColours = listOf(
     R.color.lavender,
     R.color.carnation,
